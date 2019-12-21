f783ccda2be1a987868c13e14a4617b6e291dd84
Mario <mario.dsa123@gmail.com>
2019-12-22 00:57:18 +0530

    new video

--- src/components/video.area/video.area.jsx
+++ src/components/video.area/video.area.jsx
@@ -69,10 +69,13 @@ const VideoArea = () => {
     setVolume(vol);
   };
 
+  const videoURL =
+    "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4";
+
   return (
     <div className="video-area">
       <VideoPlayer
-        url="https://4bdba003.ngrok.io/video"
+        url={videoURL}
         isPlaying={isPlaying}
         volume={volume}
         onPlay={handlePlay}
